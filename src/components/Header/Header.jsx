07dfeabbc223d5f9d898e5f324f82a6d7d5a5906
eun0leee <eun0leee@gmail.com>
2023-01-16 21:41:44 +0900

    feat: header, nav 레이아웃

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import './Header.scss';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="logo">
+        <a href="#">
+          <img src="#" alt="youtube logo" />
+        </a>
+      </div>
+      <form>
+        <input type="text" placeholder="검색" />
+        <button>키보드</button>
+        <button>
+          <img src="#" alt="search button" />
+        </button>
+      </form>
+      <button>음성인식</button>
+      <div>
+        <button>영상</button>
+        <button>알림</button>
+        <button>user</button>
+      </div>
+    </header>
+  );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import './Sidebar.scss';
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <nav>
+      <ul>
+        <button>홈</button>
+        <button>Shorts</button>
+        <button>구독</button>
+        <button>보관함</button>
+      </ul>
+    </nav>
+  );
 }
