8db80789a3245acbef41b96332a6c3a6f4fb8b84
eun0leee <eun0leee@gmail.com>
2023-01-17 19:48:58 +0900

    Docs : 헤더 스타일링

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,41 +1,46 @@
 import React from 'react';
 import './Header.scss';
+import logo from './assets/logo.png';
 
 // icons
 import { BsList, BsBell } from 'react-icons/bs';
-import { RiVideoAddLine } from 'react-icons/ri';
-import { FaRegUserCircle } from 'react-icons/fa';
+import { RiVideoAddLine, RiMicFill } from 'react-icons/ri';
+import { FaRegUserCircle, FaUserAlt } from 'react-icons/fa';
+import { IoSearchOutline } from 'react-icons/io5';
 
 export default function Header() {
   return (
     <header>
-      <div className="menu">
-        <BsList className="header-icon" size="24"></BsList>
-      </div>
-      <div className="logo">
-        <a href="#">
-          <img src="#" alt="youtube logo" />
+      <div className="header-left-logomenu">
+        <BsList className="header-icon menu" size="24"></BsList>
+        <a href="#" className="logo">
+          <img src={logo} alt="youtube logo" />
+          <sup>KR</sup>
         </a>
       </div>
-      <form>
-        <input type="text" placeholder="검색" />
+      <div className="header-search">
+        <form>
+          <input type="text" placeholder="검색" />
+          <button className="keyboard">
+            <img src="https://www.gstatic.com/inputtools/images/tia.png" alt="키보드" />
+          </button>
+        </form>
+        <button className="header-search-zoombtn">
+          <IoSearchOutline className="header-icon search"></IoSearchOutline>
+        </button>
+        <button className="header-search-voicebtn">
+          <RiMicFill className="header-icon voice"></RiMicFill>
+        </button>
+      </div>
+      <div className="header-right-btns">
         <button>
-          <img src="https://www.gstatic.com/inputtools/images/tia.png" alt="키보드" />
+          <RiVideoAddLine className="header-icon"></RiVideoAddLine>
         </button>
         <button>
-          <img src="#" alt="search button" />
-        </button>
-      </form>
-      <button>음성인식</button>
-      <div>
-        <button>
-          <RiVideoAddLine className="header-icon" size="24"></RiVideoAddLine>
+          <BsBell className="header-icon"></BsBell>
         </button>
         <button>
-          <BsBell className="header-icon" size="24"></BsBell>알림
-        </button>
-        <button>
-          <FaRegUserCircle className="header-icon user" size="32"></FaRegUserCircle>
+          <FaUserAlt className="header-icon user"></FaUserAlt>
         </button>
       </div>
     </header>
