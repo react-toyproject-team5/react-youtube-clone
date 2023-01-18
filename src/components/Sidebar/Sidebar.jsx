247e5429c97d836469231f94d29bc8f9b2f25765
eun0leee <eun0leee@gmail.com>
2023-01-18 18:43:10 +0900

    feat : 사이드바 반응형

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -10,21 +10,22 @@ import { SlArrowDown } from 'react-icons/sl';
 
 export default function Sidebar() {
   return (
-    <nav>
-      <ul>
-        <button>
-          <RiHome5Fill className="sidebar-icon" size="24"></RiHome5Fill>홈
-        </button>
-        <button>
-          <BsCollectionPlay className="sidebar-icon" size="24"></BsCollectionPlay>구독
-        </button>
-        <button>
-          <MdOutlineVideoLibrary className="sidebar-icon" size="24"></MdOutlineVideoLibrary>보관함
-        </button>
-        <button>
-          <MdOutlineRestore className="sidebar-icon" size="24"></MdOutlineRestore>시청기록
-        </button>
-        {/* <button>
+    <>
+      <nav className="small-nav">
+        <ul>
+          <button>
+            <RiHome5Fill className="sidebar-icon" size="24"></RiHome5Fill>홈
+          </button>
+          <button>
+            <BsCollectionPlay className="sidebar-icon" size="24"></BsCollectionPlay>구독
+          </button>
+          <button>
+            <MdOutlineVideoLibrary className="sidebar-icon" size="24"></MdOutlineVideoLibrary>보관함
+          </button>
+          <button>
+            <MdOutlineRestore className="sidebar-icon" size="24"></MdOutlineRestore>시청기록
+          </button>
+          {/* <button>
           <AiOutlinePlaySquare></AiOutlinePlaySquare>내 동영상
         </button>
         <button>
@@ -33,7 +34,33 @@ export default function Sidebar() {
         <button>
           <SlArrowDown></SlArrowDown>더보기
         </button> */}
-      </ul>
-    </nav>
+        </ul>
+      </nav>
+      <nav className="large-nav">
+        <ul>
+          <button>
+            <RiHome5Fill className="sidebar-icon" size="24"></RiHome5Fill>홈
+          </button>
+          <button>
+            <BsCollectionPlay className="sidebar-icon" size="24"></BsCollectionPlay>구독
+          </button>
+          <button>
+            <MdOutlineVideoLibrary className="sidebar-icon" size="24"></MdOutlineVideoLibrary>보관함
+          </button>
+          <button>
+            <MdOutlineRestore className="sidebar-icon" size="24"></MdOutlineRestore>시청기록
+          </button>
+          {/* <button>
+          <AiOutlinePlaySquare></AiOutlinePlaySquare>내 동영상
+        </button>
+        <button>
+          <MdOutlineQueryBuilder></MdOutlineQueryBuilder>나중에 볼 동영상
+        </button>
+        <button>
+          <SlArrowDown></SlArrowDown>더보기
+        </button> */}
+        </ul>
+      </nav>
+    </>
   );
 }
