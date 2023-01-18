fb6cfca1d3cc0ec9947d14d51d2ef912764bc22b
eun0leee <eun0leee@gmail.com>
2023-01-19 00:32:50 +0900

    style : 헤더 오타 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,10 @@ import React, { useState, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Tooltip } from 'react-tooltip';
 import 'react-tooltip/dist/react-tooltip.css';
+
 import styles from './Header.module.scss';
 import logo from './assets/logo.png';
+import Sidebar from '../Sidebar/Sidebar';
 
 // icons
 import { BsList, BsBell } from 'react-icons/bs';
@@ -15,6 +17,7 @@ export default function Header() {
   const navigate = useNavigate();
   const [inputValue, setInputValue] = useState('');
   const inputRef = useRef('');
+  const [sidebarDrop, setSidebarDrop] = useState(false);
 
   const getSearchValue = (e) => {
     e.preventDefault();
@@ -24,44 +27,51 @@ export default function Header() {
     // setInputValue(inputRef.current.value);
   };
 
+  const sidebarClick = (e) => {
+    setSidebarDrop((e) => !e);
+  };
+
   return (
-    <header>
-      <div className={styles.headerLeftLogomenu}>
-        <BsList className={styles.headerIcon} size="24"></BsList>
-        <Link to={'/'} className={styles.logo}>
-          <img src={logo} alt="youtube logo" />
-          <sup>KR</sup>
-        </Link>
-      </div>
-      <div className={styles.headerSearch}>
-        <form onSubmit={getSearchValue}>
-          <input type="text" placeholder="검색" ref={useRef('')} />
-          <button className={styles.keyboard}>
-            <img src="https://www.gstatic.com/inputtools/images/tia.png" alt="키보드" />
+    <>
+      <header>
+        <div className={styles.headerLeftLogomenu}>
+          <BsList className={styles.headerIcon} size="24" onClick={sidebarClick}></BsList>
+          <Link to={'/'} className={styles.logo}>
+            <img src={logo} alt="youtube logo" />
+            <sup>KR</sup>
+          </Link>
+        </div>
+        <div className={styles.headerSearch}>
+          <form onSubmit={getSearchValue}>
+            <input type="text" placeholder="검색" ref={useRef('')} />
+            <button className={styles.keyboard}>
+              <img src="https://www.gstatic.com/inputtools/images/tia.png" alt="키보드" />
+            </button>
+          </form>
+          <button className={styles.headerSearchZoombtn} id="tooltip-search">
+            <IoSearchOutline className={`${styles.headerIcon} ${styles.search}`}></IoSearchOutline>
           </button>
-        </form>
-        <button className={styles.headerSearchZoombtn} id="tooltip-search">
-          <IoSearchOutline className={`${styles.headerIcon} ${styles.search}`}></IoSearchOutline>
-        </button>
-        <Tooltip anchorId="tooltip-search" content="검색" className={styles.tooltip} />
-        <button className={styles.headerSearchVoicebtn} id="tooltip-voice">
-          <RiMicFill className={`${styles.headerIcon} ${styles.voice}`}></RiMicFill>
-        </button>
-        <Tooltip anchorId="tooltip-voice" content="음성으로 검색" className={styles.tooltip} />
-      </div>
-      <div className={styles.headerRightBtns}>
-        <button id="tooltip-upload">
-          <RiVideoAddLine className={styles.headerIcon}></RiVideoAddLine>
-        </button>
-        <Tooltip anchorId="tooltip-upload" content="만들기" className={styles.tooltip} />
-        <button id="tooltip-notice">
-          <BsBell className={styles.headerIcon}></BsBell>
-        </button>
-        <Tooltip anchorId="tooltip-notice" content="알림" className={styles.tooltip} />
-        <button>
-          <FaUserAlt className={`${styles.headerIcon} ${styles.user}`}></FaUserAlt>
-        </button>
-      </div>
-    </header>
+          <Tooltip anchorId="tooltip-search" content="검색" className={styles.tooltip} />
+          <button className={styles.headerSearchVoicebtn} id="tooltip-voice">
+            <RiMicFill className={`${styles.headerIcon} ${styles.voice}`}></RiMicFill>
+          </button>
+          <Tooltip anchorId="tooltip-voice" content="음성으로 검색" className={styles.tooltip} />
+        </div>
+        <div className={styles.headerRightBtns}>
+          <button id="tooltip-upload">
+            <RiVideoAddLine className={styles.headerIcon}></RiVideoAddLine>
+          </button>
+          <Tooltip anchorId="tooltip-upload" content="만들기" className={styles.tooltip} />
+          <button id="tooltip-notice">
+            <BsBell className={styles.headerIcon}></BsBell>
+          </button>
+          <Tooltip anchorId="tooltip-notice" content="알림" className={styles.tooltip} />
+          <button>
+            <FaUserAlt className={`${styles.headerIcon} ${styles.user}`}></FaUserAlt>
+          </button>
+        </div>
+      </header>
+      <Sidebar drop={sidebarDrop} />
+    </>
   );
 }
