25d88f0b441844d83e5a6b00904e3b748b93c39b
조승후 <choseunghu@joseunghuui-MacBookAir.local>
2023-01-19 22:32:36 +0900

    Feat: Add player components for VideoDetails

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import './App.css';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Comment from './components/Comments/Comments';
-import RelatedVideoInVideoDetail from './components/RelatedVideoInVideoDetail/RelatedVideoInVideoDetail';
+import VideoDetail from './pages/VideoDetail';
 // 페이지 공통적으로 들어가야 할 components 있어야 함
 // ex) header, sidebar
 
 const queryClient = new QueryClient();
-
 function App() {
   return (
     <>
       <Header />
       {/* <Sidebar /> */}
-      <div className="VideoDetail">
-        <Comment />
-        <RelatedVideoInVideoDetail />
-      </div>
       <Outlet />
       <QueryClientProvider client={queryClient}>
         <Outlet />
