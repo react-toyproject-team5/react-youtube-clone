df4f86c29e919076726d7e8e6c792396f3ed861f
eun0leee <eun0leee@gmail.com>
2023-01-19 16:04:39 +0900

    Design : 사이드바 확장형 스타일 수정

--- src/components/Sidebar/SidebarLarge.jsx
+++ src/components/Sidebar/SidebarLarge.jsx
@@ -136,8 +136,9 @@ export default function SidebarLarge() {
       <footer>
         © 2023 Google LLC, Sundar Pichai, 1600 Amphitheatre Parkway, Mountain View CA 94043, USA, 0807-882-594 (무료),
         yt-support-solutions-kr@google.com, 호스팅: Google LLC, <a href="#!">사업자정보</a>,{' '}
-        <a href="#!">불법촬영물 신고</a> 크리에이터들이 유튜브 상에 게시, 태그 또는 추천한 상품들은 판매자들의 약관에
-        따라 판매됩니다. 유튜브는 이러한 제품들을 판매하지 않으며, 그에 대한 책임을 지지 않습니다.
+        <a href="#!">불법촬영물 신고</a> <br />
+        크리에이터들이 유튜브 상에 게시, 태그 또는 추천한 상품들은 판매자들의 약관에 따라 판매됩니다. 유튜브는 이러한
+        제품들을 판매하지 않으며, 그에 대한 책임을 지지 않습니다.
       </footer>
     </nav>
   );
