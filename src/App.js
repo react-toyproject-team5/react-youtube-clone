3f5a83c0a6e9bdf27958c3be77a6e13e49251136
eun0leee <eun0leee@gmail.com>
2023-01-19 20:28:40 +0900

    Design : 헤더, 사이드바, 아웃렛 전체 레이아웃 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React, { useState } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// components
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Comment from './components/Comments/Comments';
 import RelatedVideoInVideoDetail from './components/RelatedVideoInVideoDetail/RelatedVideoInVideoDetail';
+
 // 페이지 공통적으로 들어가야 할 components 있어야 함
 // ex) header, sidebar
 
@@ -23,11 +26,9 @@ function App() {
         <Header menuDrop={menuDrop} setMenuDrop={setMenuDrop} menuSlide={menuSlide} setMenuSlide={setMenuSlide} />
         <div style={{ display: 'flex' }}>
           {location.pathname === '/watch/:videoId' ? null : <Sidebar menuDrop={menuDrop} menuSlide={menuSlide} />}
-          {/* <div className="VideoDetail">
-            <Comment />
-            <RelatedVideoInVideoDetail />
-          </div> */}
-          <Outlet />
+          <div className="outlet-layout">
+            <Outlet />
+          </div>
         </div>
       </QueryClientProvider>
     </>
