b38018a23f48cffda6224b7bc51bfc0c628f5f01
Kim JiYeong <zmswkfl@naver.com>
2023-01-20 14:05:37 +0900

    feat : 무한스크롤 testing

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,54 +1,70 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
-
+import { useInView } from 'react-intersection-observer';
 import VideoCard from '../components/mainPage/VideoCard';
-// import koLocale from '../../node_modules/timeago.js/lib/lang/ko.js';
 
 export default function Videos() {
   const [videoData, setVideoData] = useState([]);
-
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
   const [listOpen, setListOpen] = useState('');
+  const [page, setPage] = useState('');
+  const [ref, inView] = useInView();
+  const [data, setData] = useState({});
 
-  const getVideo = async () => {
-    try {
-      setError('');
-      setVideoData([]);
-      setLoading(true);
-
-      const video = await axios.get('http://localhost:4000/file');
-
-      console.log(video.data.items);
-      setVideoData(video.data.items);
-    } catch (error) {
-      setError(error);
-      console.log(error);
-    }
+  const getVideo = useCallback(async () => {
+    setVideoData([]);
+    setLoading(true);
+    await axios
+      .get(
+        `https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=25&key=AIzaSyDGkRdsPIgDMjduzvBSidnZbn6Th3_0QtE${page}`,
+        // `http://localhost:4000/file`,
+      )
+      .then((response) => {
+        setVideoData((prev) => [...prev, ...response.data.items]);
+      });
+    const video = await axios.get(
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=25&key=AIzaSyDGkRdsPIgDMjduzvBSidnZbn6Th3_0QtE${page}`,
+      // `http://localhost:4000/file`,
+    );
+    setData(video.data);
     setLoading(false);
-  };
+  }, [page]);
 
   useEffect(() => {
     getVideo();
-  }, []);
+  }, [getVideo]);
+
+  useEffect(() => {
+    if (inView && !loading) {
+      setPage(`&pageToken=${data.nextPageToken}`);
+    }
+  }, [inView, loading]);
 
   if (loading) {
     return <div>로딩 중...</div>;
   }
-  if (error) {
-    return <div>에러가 발생했습니다.</div>;
-  }
+
   if (videoData === []) {
     return <div></div>;
   }
 
-  // console.log(listOpen);
+  console.log(videoData);
 
   return (
     <div>
       <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-        {videoData.map((videoCard) => (
-          <VideoCard listOpen={listOpen} setListOpen={setListOpen} videoCard={videoCard} key={videoCard.id} />
+        {videoData.map((videoCard, index) => (
+          <React.Fragment key={videoCard.id}>
+            {videoData.length - 5 === index ? (
+              <div ref={ref}>
+                <VideoCard listOpen={listOpen} setListOpen={setListOpen} videoCard={videoCard} />
+              </div>
+            ) : (
+              <div>
+                <VideoCard listOpen={listOpen} setListOpen={setListOpen} videoCard={videoCard} />
+              </div>
+            )}
+          </React.Fragment>
         ))}
       </div>
     </div>
