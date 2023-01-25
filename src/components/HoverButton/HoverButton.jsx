3aac9fd9ddfbfb23a93c441a9bd21f92b550b1a8
Wonny-ing <h.wonny11@gmail.com>
2023-01-25 21:21:07 +0900

    feat: Create auto player

---
+++ src/components/HoverButton/HoverButton.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './HoverButton.module.scss';
+import { MdOutlineQueueMusic } from 'react-icons/md';
+import { BiTimeFive, BiListPlus } from 'react-icons/bi';
+import { RiShareForwardLine } from 'react-icons/ri';
+import { MdOutlineFlag } from 'react-icons/md';
+
+export default function HoverButton() {
+  return (
+    <ul className={styles.list}>
+      <li className={styles.card}>
+        <MdOutlineQueueMusic className={styles.icon} />
+        <p>현재 재생목록에 추가</p>
+      </li>
+      <li className={styles.card}>
+        <BiTimeFive className={styles.icon} />
+        <p>나중에 볼 동영상에 저장</p>
+      </li>
+      <li className={styles.card}>
+        <BiListPlus className={styles.icon} />
+        <p>재생목록에 저장</p>
+      </li>
+      <li className={styles.card}>
+        <RiShareForwardLine className={styles.icon} />
+        <p>공유</p>
+      </li>
+      <li className={styles.card}>
+        <MdOutlineFlag className={styles.icon} />
+        <p>신고</p>
+      </li>
+    </ul>
+  );
+}
