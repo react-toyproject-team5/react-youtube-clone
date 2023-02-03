8829c41ab229b5ddaf631fd0a75e36c416ae555a
eun0leee <eun0leee@gmail.com>
2023-02-03 22:08:42 +0900

    Refactor : 커스텀 훅 사용

--- src/components/Layout/Sidebar/SidebarModal.jsx
+++ src/components/Layout/Sidebar/SidebarModal.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import styles from './Sidebar.module.scss';
+import useOnClickOutside from '../../../hooks/useOnClickOutside';
 
 // Components
 import SidebarLarge from './SidebarLarge';
@@ -7,26 +8,7 @@ import HeaderMenu from '../Header/HeaderMenu';
 
 export default function SidebarModal({ modal, setModal }) {
   const modalRef = useRef(null);
-
-  useEffect(() => {
-    // 이벤트 핸들러 함수
-    const clickDocument = (e) => {
-      // mousedown 이벤트가 발생한 영역이 모달창이 아닐 때, 모달창 제거 처리
-      if (modalRef.current && !modalRef.current.contains(e.target)) {
-        setModal(false);
-      }
-    };
-
-    // 이벤트 핸들러 등록
-    document.addEventListener('mousedown', clickDocument);
-    document.addEventListener('touchstart', clickDocument); // 모바일 대응
-
-    return () => {
-      // 이벤트 핸들러 해제
-      document.removeEventListener('mousedown', clickDocument);
-      document.removeEventListener('touchstart', clickDocument); // 모바일 대응
-    };
-  });
+  useOnClickOutside(modalRef, () => setModal(false));
 
   return (
     <nav className={modal ? `${styles.modalNav} ${styles.open}` : styles.modalNav} ref={modalRef}>
