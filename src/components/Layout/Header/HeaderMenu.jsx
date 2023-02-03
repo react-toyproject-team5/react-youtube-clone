10f07a99edb99a072f0d2c92124c8ae7fd78cb1e
eun0leee <eun0leee@gmail.com>
2023-02-03 14:50:30 +0900

    Refactor: 헤더 메뉴로고영역 컴포넌트 분리

---
+++ src/components/Layout/Header/HeaderMenu.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.scss';
+
+import logo from './assets/logo.png';
+import { BsList } from 'react-icons/bs';
+
+export default function HeaderMenu({ setModal, setSidebar, menuBtn }) {
+  console.log(menuBtn);
+  const btnClick = () => {
+    switch (menuBtn) {
+      case 'openModal':
+        // 태블릿과 모바일 사이즈, 메뉴 버튼으로 모달 사이드바 열기
+        (function openModal(e) {
+          setModal((e) => !e);
+        })();
+        break;
+      case 'openSidebar':
+        // 데스크탑 사이즈, 메뉴 버튼 클릭
+        (function openSidebar(e) {
+          setSidebar((e) => !e);
+        })();
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <>
+      <BsList className={styles.headerIcon} size="24" onClick={btnClick} />
+      <Link to={'/'} className={styles.logo}>
+        <img src={logo} alt="youtube logo" />
+        <sup>KR</sup>
+      </Link>
+    </>
+  );
+}
